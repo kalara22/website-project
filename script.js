a58b62a36a498849eada6d3157a5e9abda4424e4
kate sandoval <kate.sandovalrivea200@gmail.com>
2026-08-30 14:00:01 -0700

    refactor contact form: update input IDs and add error handling for name validation

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-const styleTextBox = document.querySelector("#name")
-
+const styleTextBox = document.querySelector("#names-id-input")
 styleTextBox.addEventListener("input", () => {
     styleTextBox.classList.add("text-input")
 })
 
-const nameInput = document.querySelector("#name");
+const styleTextBox2 = document.querySelector("#email-id-input");
+styleTextBox.addEventListener("input", () => {
+    styleTextBox.classList.add("text-input2");
+}) 
+
+const nameInput = document.querySelector("#names");
 const contactForm = document.querySelector("#contact-form");
 const errorMessage = document.querySelector("#name-error");
 
 contactForm.addEventListener("submit", (event) =>{
     event.preventDefault();
 
-    if(nameInput.value === ""){
+    if(nameInput.value.trim === ""){
        errorMessage.textContent = "Error: Name cannot be empty";
     } else{
         console.log(`accepted: ${nameInput.value}`);
             nameInput.value = "";
+            errorMessage.textContent = "";
     }
 })
 
